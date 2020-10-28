b765675c9a88c92dc6b7cd0c830299ce08a7173d
jgabari <j.gabari.2017@alumnos.urjc.es>
2020-10-28 20:45:07 +0100

    empezado el ejer 3

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,2 +1,90 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+
+class SmallSMILHandler(ContentHandler):
+    """
+    Clase para manejar SMIL
+    """
+
+    def __init__(self):
+        """
+        Constructor. Inicializamos las variables
+        """
+        self.width = ""
+        self.height = ""
+        self.backgroundcolor = ""
+        self.id = ""
+        self.top = ""
+        self.bottom = ""
+        self.left = ""
+        self.right = ""
+        self.src = ""
+        self.region = ""
+        self.begin = ""
+        self.dur = ""
+
+
+    def startElement(self, name, attrs):
+        """
+        Método que se llama cuando se abre una etiqueta
+        """
+        if name == 'root-layout':
+            # De esta manera tomamos los valores de los atributos
+            self.width = attrs.get('width', "")
+            self.height = attrs.get('height', "")
+            self.backgroundcolor = attrs.get('background-color', "")
+        elif name == 'region':
+            self.id = attrs.get('id', "")
+            self.top = attrs.get('top', "")
+            self.bottom = attrs.get('bottom', "")
+            self.left = attrs.get('left', "")
+            self.right = attrs.get('right', "")
+        elif name == 'img':
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
+        elif name == 'audio':
+            self.src = attrs.get('src', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
+        elif name == 'textstream':
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+
+    def endElement(self, name):
+        """
+        Método que se llama al cerrar una etiqueta
+        """
+        if name == 'root-layout':
+            # Guardar el contenido en una variable self.content
+            self.pregunta = ""
+        if name == 'region':
+            self.respuesta = ""
+        if name == 'img':
+            self.pregunta = ""
+        if name == 'audio':
+            self.respuesta = ""
+        if name == 'textstream':
+            self.respuesta = ""
+
+
+    def get_content(self):
+        """
+        Método que consigue el contenido
+        """
+
+
+
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = ChistesHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('chistes2.xml'))

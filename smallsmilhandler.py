4a7f21e0c743962841cd3a7bb21f93800c2a36f9
jgabari <j.gabari.2017@alumnos.urjc.es>
2020-11-04 18:19:40 +0100

    Eliminado end_element y cambiado start_element para que el código sea más corto

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -33,54 +33,32 @@ class SmallSMILHandler(ContentHandler):
         Método que se llama cuando se abre una etiqueta
         """
         if name == 'root-layout':
-            # De esta manera tomamos los valores de los atributos
-            self.width = attrs.get('width', "")
-            self.height = attrs.get('height', "")
-            self.backgroundcolor = attrs.get('background-color', "")
+            self.content.append(name)
+            self.content.append({'width': attrs.get('width', ""),
+                                 'height': attrs.get('height', ""),
+                                 'background-color': attrs.get('background-color', "")})
         elif name == 'region':
-            self.id = attrs.get('id', "")
-            self.top = attrs.get('top', "")
-            self.bottom = attrs.get('bottom', "")
-            self.left = attrs.get('left', "")
-            self.right = attrs.get('right', "")
+            self.content.append(name)
+            self.content.append({'id': attrs.get('id', ""),
+                                 'top': attrs.get('top', ""),
+                                 'bottom': attrs.get('bottom', ""),
+                                 'left': attrs.get('left', ""),
+                                 'right': attrs.get('right', "")})
         elif name == 'img':
-            self.src = attrs.get('src', "")
-            self.region = attrs.get('region', "")
-            self.begin = attrs.get('begin', "")
-            self.dur = attrs.get('dur', "")
+            self.content.append(name)
+            self.content.append({'src': attrs.get('src', ""),
+                                 'region': attrs.get('region', ""),
+                                 'begin': attrs.get('begin', ""),
+                                 'dur': attrs.get('dur', "")})
         elif name == 'audio':
-            self.src = attrs.get('src', "")
-            self.begin = attrs.get('begin', "")
-            self.dur = attrs.get('dur', "")
+            self.content.append(name)
+            self.content.append({'src': attrs.get('src', ""),
+                                 'begin': attrs.get('begin', ""),
+                                 'dur': attrs.get('dur', "")})
         elif name == 'textstream':
-            self.src = attrs.get('src', "")
-            self.region = attrs.get('region', "")
-
-    def endElement(self, name):
-        """
-        Método que se llama al cerrar una etiqueta
-        """
-        if name == 'root-layout':
-            # Guardar el contenido en una variable self.content
             self.content.append(name)
-            self.content.append({'width': self.width, 'height': self.height,
-                                 'background-color': self.backgroundcolor})
-        if name == 'region':
-            self.content.append(name)
-            self.content.append({'id': self.id, 'top': self.top,
-                                 'bottom': self.bottom, 'left': self.left,
-                                 'right': self.right})
-        if name == 'img':
-            self.content.append(name)
-            self.content.append({'src': self.src, 'region': self.region,
-                                 'begin': self.begin, 'dur': self.dur})
-        if name == 'audio':
-            self.content.append(name)
-            self.content.append({'src': self.src, 'begin': self.begin,
-                                 'dur': self.dur})
-        if name == 'textstream':
-            self.content.append(name)
-            self.content.append({'src': self.src, 'region': self.region})
+            self.content.append({'src': attrs.get('src', ""),
+                                 'region': attrs.get('region', "")})
 
     def get_tags(self):
         """
